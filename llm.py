b74af191246a1c29c34cf3b07f8375d4cc9f0fd3
paulkr-sketch <ravishpaulkr@gmail.com>
2026-06-10 18:08:42 +0530

    fix secrets key

--- llm.py
+++ llm.py
@@ -1,12 +1,11 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-import streamlit as st
 import os
 
 load_dotenv()
 
 llm_model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key=st.secrets["GOOGLE_API_KEY"]
+    google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
